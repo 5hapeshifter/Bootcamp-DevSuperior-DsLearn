484ca5af352d387fb92a394c59460d64b8378e17
5hapeshifter <suleimandiverio@gmail.com>
2022-06-15 10:54:30 -0300

    Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,13 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_offer")
 public class Offer implements Serializable {
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,6 +28,9 @@ public class Offer implements Serializable {
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
+
+    @OneToMany(mappedBy = "offer")
+    private List<Resource> resources = new ArrayList<>();
 
     public Offer() {
     }
@@ -38,45 +43,39 @@ public class Offer implements Serializable {
         this.endMoment = endMoment;
         this.course = course;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getEdition() {
         return edition;
     }
-
     public void setEdition(String edition) {
         this.edition = edition;
     }
-
     public Instant getStartMoment() {
         return startMoment;
     }
-
     public void setStartMoment(Instant startMoment) {
         this.startMoment = startMoment;
     }
-
     public Instant getEndMoment() {
         return endMoment;
     }
-
     public void setEndMoment(Instant endMoment) {
         this.endMoment = endMoment;
     }
-
     public Course getCourse() {
         return course;
     }
-
     public void setCourse(Course course) {
         this.course = course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
     }
 
     @Override
@@ -86,7 +85,6 @@ public class Offer implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
