c22c2648b6af31322296e6682be87a4500471102
5hapeshifter <suleimandiverio@gmail.com>
2022-06-21 11:50:44 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -45,6 +46,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtTokenEnhancer tokenEnhancer;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         // estamos inserindo o nome do metodo
@@ -58,8 +62,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient(clientId) // aqui é a identificação do clientId, o front end deve informar esse dado identico para acessar a nossa aplicacao
                 .secret(passwordEncoder.encode(clientSecret)) // senha da aplicacao
                 .scopes("read", "write") // permissões de acesso
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(jwtDuration); // tempo de um dia de validade do token
+                .authorizedGrantTypes("password", "refresh_token") // tipos de chaves aceitas
+                .accessTokenValiditySeconds(jwtDuration) // tempo de um dia de validade do token
+                .refreshTokenValiditySeconds(jwtDuration); // tempo de duracao do novo token
     }
 
     // Metodo responsavel por gerenciar a autorizacao e o formato do token
@@ -73,6 +78,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(tokenStore)
                 .accessTokenConverter(accessTokenConverter)
-                .tokenEnhancer(chain);
+                .tokenEnhancer(chain)
+                .userDetailsService(userDetailsService);
     }
 }
